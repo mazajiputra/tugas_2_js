90ae3206f50bf6082796314357e6494d265cd894
mazajiputra <mazajiputra@gmail.com>
2021-10-29 18:46:28 +0700

    Kumpul Tugas

---
+++ tugas2.js
@@ -0,0 +1,17 @@
+//1. number
+angka=100;
+//2. string
+kalimat="Indonesia";
+//3. boolean
+lean=true;
+//4. null atau undifined
+kosong=null;
+//5. array
+huruf=["a","b","c","d","e"];
+
+console.log(angka);
+console.log(kalimat);
+console.log(lean);
+console.log(kosong);
+console.log(huruf);
+
